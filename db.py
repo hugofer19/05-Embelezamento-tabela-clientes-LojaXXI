ebba78d97f3d9ef6dd740924a5d2c8431bfa006b
Hugo Ferreira <46359390+hugofer19@users.noreply.github.com>
2021-05-22 00:08:01 +0100

    Add files via upload

---
+++ db.py
@@ -0,0 +1,6 @@
+class Database:
+    def __init__(self):
+        self.Host = 'ec2-54-74-14-109.eu-west-1.compute.amazonaws.com'
+        self.Database = 'd8i5e964chmjgj'
+        self.User = 'psgtekdhofssij'
+        self.Password = '04e85cf222296a32c40d9354d7aa92d5b1a7c961a994f0f5c67faa9f1941e46c'
---
+++ user.py
@@ -0,0 +1,79 @@
+import psycopg2
+class User:
+    db = 'db/Utilizadores.xlsx'
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.id = None
+        self.cliente = ''
+        self.email = ''
+        self.password = ''
+        self.nif = ''
+        self.nome = ''
+        self.morada = ''
+
+    def herokudb(self):
+        from db import Database
+        mydb = Database()
+        return psycopg2.connect(host=mydb.Host, database=mydb.Database, user=mydb.User, password=mydb.Password, sslmode='require')
+
+    def gravar(self, login, email, password):
+        ficheiro = self.herokudb()
+        db = ficheiro.cursor()
+        #db.execute("drop table usr")
+        db.execute("CREATE TABLE IF NOT EXISTS usr (id serial primary key, login text,email text, passe text, nif text, nome text, morada char(60))")
+        db.execute("INSERT INTO usr VALUES (DEFAULT ,%s, %s, %s)", (login, email, self.code(password),))
+        ficheiro.commit()
+        ficheiro.close()
+
+    def existe(self, login):
+        try:
+            ficheiro = self.herokudb()
+            db = ficheiro.cursor()
+            db.execute("SELECT * FROM usr WHERE login = %s", (login,))
+            valor = db.fetchone()
+            ficheiro.close()
+        except:
+            valor = None
+        return valor
+
+    def log(self,login, password):
+        ficheiro = self.herokudb()
+        db = ficheiro.cursor()
+        db.execute("SELECT * FROM usr WHERE login = %s and passeword = %s", (login, self.code(password),))
+        valor = db.fetchone()
+        ficheiro.close()
+        return valor
+
+    def alterar(self, login, password):
+        ficheiro = self.herokudb()
+        db = ficheiro.cursor()
+        db.execute("UPDATE usr SET password = %s WHERE login = %s", (self.code(password), login))
+        ficheiro.commit()
+        ficheiro.close()
+
+    def apaga(self, login):
+        ficheiro = self.herokudb()
+        db = ficheiro.cursor()
+        db.execute("DELETE FROM usr WHERE login = %s", (login,))
+        ficheiro.commit()
+        ficheiro.close()
+
+    def lista(self):
+        try:
+            ficheiro = self.herokudb()
+            db = ficheiro.cursor()
+            db.execute("select * from usr")
+            valor = db.fetchall()
+            ficheiro.close()
+        except:
+            valor = None
+        return valor
+
+    @staticmethod
+    def code(passe):
+        import hashlib
+        return hashlib.sha3_256(passe.encode()).hexdigest()
+
